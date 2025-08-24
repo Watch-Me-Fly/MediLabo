a431ac59ace550927b0246da98f3ae9e30fc1951
watch-me-fly <thi.phoned@gmail.com>
2025-08-24 23:50:21 +0200

    add error management and logging in patiebt service class

--- patient-service/src/main/java/com/oc/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/oc/patientservice/service/PatientService.java
@@ -2,6 +2,8 @@ package com.oc.patientservice.service;
 
 import com.oc.patientservice.model.Patient;
 import com.oc.patientservice.repository.PatientRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -10,6 +12,7 @@ import java.util.Optional;
 
 @Service
 public class PatientService {
+    Logger log = LoggerFactory.getLogger(PatientService.class);
     private final PatientRepository repository;
 
     public PatientService(PatientRepository repository) {
@@ -18,6 +21,7 @@ public class PatientService {
 
     // create
     public void createPatient(Patient patient) {
+        log.info("[PatientService] - entered createPatient");
         if (patient.getFirstName() == null
                 || patient.getFirstName().isEmpty()) {
             throw new IllegalArgumentException("first name is mandatory");
@@ -36,33 +40,69 @@ public class PatientService {
                 || patient.getSex().isEmpty()) {
             throw new IllegalArgumentException("sex is mandatory");
         }
-        repository.save(patient);
+        try {
+            repository.save(patient);
+            log.info("[PatientService] - Exit createPatient");
+        }
+        catch (Exception e) {
+            log.error("[PatientService] - error creating patient :" + e);
+        }
     }
     // read
     public List<Patient> findAll() {
-        return repository.findAll();
+        log.info("[PatientService] - entered findAll");
+        try {
+            return repository.findAll();
+        } catch (Exception e) {
+            log.error("[PatientService] - error finding all patients");
+            throw new RuntimeException("Error retrieving all patients list : " + e);
+        }
     }
     public Optional<Patient> findById(String id) {
-        return repository.findById(id);
+        log.info("[PatientService] - entered findById");
+        try {
+            return repository.findById(id);
+        } catch (Exception e) {
+            log.error("[PatientService] - error finding patient by id :" + e);
+            throw new RuntimeException("Error retrieving patient by id :" + e);
+        }
     }
     public Optional<Patient> getPatientInformation(String firstName,
                                                    String lastName,
                                                    LocalDate dateOfBirth) {
-        return repository.findByFirstNameAndLastNameAndBirthDate(
-                firstName, lastName, dateOfBirth);
+        log.info("[PatientService] - entered getPatientInformation");
+        try {
+            return repository.findByFirstNameAndLastNameAndBirthDate(
+                    firstName, lastName, dateOfBirth);
+        } catch (Exception e) {
+            log.error("[PatientService] - error getting patient information :" + e);
+            throw new RuntimeException("Error retrieving patient information :" + e);
+        }
     }
     // update
     public void updatePatient(Patient patient) {
+        log.info("[PatientService] - entered updatePatient");
         if (repository.existsById(patient.getId())) {
-            repository.save(patient);
+            try {
+                repository.save(patient);
+                log.info("[PatientService] - exit updatePatient");
+            } catch (Exception e) {
+                log.error("[PatientService] - error updating patient :" + e);
+            }
         } else {
             throw new IllegalArgumentException("Patient not found");
         }
     }
     // delete
     public void deletePatient(String id) {
+        log.info("[PatientService] - entered deletePatient");
         if (repository.existsById(id)) {
-            repository.deleteById(id);
+            try {
+                repository.deleteById(id);
+                log.info("[PatientService] - exit deletePatient");
+            } catch (Exception e) {
+                log.error("[PatientService] - error deleting patient :" + e);
+            }
         } else {
             throw new IllegalArgumentException("Patient not found");
         }
