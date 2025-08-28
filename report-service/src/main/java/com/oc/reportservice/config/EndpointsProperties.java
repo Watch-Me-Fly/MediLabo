afbd701bc2f44a7b1f9235bd2300e29abe419289
watch-me-fly <thi.phoned@gmail.com>
2025-08-28 15:44:13 +0200

    connect to patient-service and note-service endpoints

---
+++ report-service/src/main/java/com/oc/reportservice/config/EndpointsProperties.java
@@ -0,0 +1,12 @@
+package com.oc.reportservice.config;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class EndpointsProperties {
+
+    private String patientService;
+    private String notesService;
+
+}
