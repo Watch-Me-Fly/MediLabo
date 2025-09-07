70b7c661fb5b29f6feea0d9922eac976b9fc3235
watch-me-fly <thi.phoned@gmail.com>
2025-09-07 17:08:31 +0200

    add error message to searching a non-existing patient

--- frontend/src/main/java/com/oc/frontend/controllers/patients/SearchPatientController.java
+++ frontend/src/main/java/com/oc/frontend/controllers/patients/SearchPatientController.java
@@ -2,16 +2,22 @@ package com.oc.frontend.controllers.patients;
 
 import com.oc.frontend.config.EndpointsProperties;
 import com.oc.frontend.models.Patient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/patients")
 public class SearchPatientController {
+
+    @Value("${gateway.base-url}")
+    private String baseUrl;
 
     private final RestTemplate restTemplate;
     private final EndpointsProperties endpoints;
@@ -22,7 +28,8 @@ public class SearchPatientController {
     }
 
     @GetMapping("/search")
-    public String showSearchForm() {
+    public String showSearchForm(Model model) {
+        model.addAttribute("errorMsg");
         return "patient/search";
     }
 
@@ -35,14 +42,16 @@ public class SearchPatientController {
         String url = endpoints.getPatientService()
                 + "/search?firstName={firstName}&lastName={lastName}&dateOfBirth={birthDate}";
 
-        Patient patient = restTemplate.getForObject(url, Patient.class,
-                firstName, lastName, birthDate);
+        try {
+            Patient patient = restTemplate.getForObject(url, Patient.class,
+                    firstName, lastName, birthDate);
+            return "redirect:" + baseUrl + "/patients/" + patient.getId();
 
-        if (patient == null || patient.getId() == null) {
-            redirectAttributes.addFlashAttribute("error", "No patient found");
-            return "redirect:/patients/search?error=notfound";
+        } catch (HttpClientErrorException.NotFound e) {
+            redirectAttributes.addFlashAttribute("errorMsg",
+                    "Patient does not exist on our database");
+                return "redirect:" + baseUrl + "/patients/search";
         }
 
-        return "redirect:/patients/" + patient.getId();
     }
 }
