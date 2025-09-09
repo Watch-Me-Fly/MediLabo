29f8158a28ca2692c25cb7862cc00f0c2c4314e9
watch-me-fly <thi.phoned@gmail.com>
2025-09-10 00:15:21 +0200

    adapt classes to change of database

--- patient-service/src/main/java/com/oc/patientservice/repository/PatientRepository.java
+++ patient-service/src/main/java/com/oc/patientservice/repository/PatientRepository.java
@@ -1,16 +1,15 @@
 package com.oc.patientservice.repository;
 
 import com.oc.patientservice.model.Patient;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
 @Repository
-public interface PatientRepository extends MongoRepository<Patient, String> {
-
+public interface PatientRepository extends JpaRepository<Patient, Long> {
     Optional<Patient> findByFirstNameAndLastNameAndBirthDate(String firstName,
-                                                               String lastName,
-                                                               LocalDate dateOfBirth);
+                                                             String lastName,
+                                                             LocalDate dateOfBirth);
 }
